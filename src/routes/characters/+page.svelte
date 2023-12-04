02e70a495b6332da3c8826b5cbe178d048e3c5d7
atzaras <alkiviadistzaras@yahoo.gr>
2023-12-04 17:02:07 +0100

    finished initial routing

---
+++ src/routes/characters/+page.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	export let data;
+
+	$: characters = data?.characters;
+</script>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>Characters</h1>
+
+<ul>
+	{#each characters as { name, id, image, occupation }}
+		<li>
+			<a href={`/characters/${id}`} class="picture">
+				<img src={image} alt={name} />
+			</a>
+
+			<div>
+				<a href={`/characters/${id}`}><b>{name}</b></a>
+
+                <p class="occupation">
+                    {occupation ? occupation : 'Unknown'}
+                </p>
+			</div>
+		</li>
+	{/each}
+</ul>
+
+<a href="/">Home</a>
+
+
+<style>
+	li {
+		display: flex;
+		gap: 2rem;
+		margin-bottom: 2rem;
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	.picture {
+		display: flex;
+		height: 100px;
+		width: 100px;
+	}
+
+	img {
+		height: 100%;
+		width: 100%;
+		object-fit: contain;
+	}
+
+	.occupation {
+		color: grey;
+	}
+</style>
---
+++ src/routes/characters/[id]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Character from '$lib/components/Character.svelte';
+	export let data;
+
+	$: character = data?.character;
+</script>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<Character {...character} />
+
+<style>
+</style>
